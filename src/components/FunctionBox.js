c7cf84f954e306058dc8a91162281633e6ff3763
unknown <calvinlu23@gmail.com>
2021-07-24 18:39:37 -0400

    Renamed and added usage descriptions where applicable to buttons in the function box

--- src/components/FunctionBox.js
+++ src/components/FunctionBox.js
@@ -8,41 +8,61 @@ const FunctionBox = ({ addElement, deleteSelectedElements, elements, quickSort, 
         <Draggable bounds="body" nodeRef={nodeRef}>
             <div className="function-box" ref={nodeRef}>
                 <h3>Data Structures</h3>
-                <Button className="btn-dark function-box-button" onClick={() => selectStructure('array')}>Array</Button>
-                <Button className="btn-dark function-box-button" onClick={() => selectStructure('graph')}>Graph</Button>
+                <Button className="btn-dark function-box-button" onClick={() => selectStructure('array')}>
+                    Array
+                </Button>
+                <Button className="btn-dark function-box-button" onClick={() => selectStructure('graph')}>
+                    Graph
+                </Button>
                 <h3>Functions</h3>
-                <Button className="function-box-button" onClick={addElement}>+ Add Element</Button>
-                <Button className="btn-danger function-box-button" onClick={deleteSelectedElements}>- Delete Selected Elements</Button>
-                <Button className="btn-danger function-box-button" onClick={clearCanvas}>Clear Canvas and Record</Button>
-                <Button className="btn-info function-box-button" onClick={toggleRecord}>{displayRecord ? "Hide Record" : "Show Record"}</Button>
+                <Button className="function-box-button" onClick={addElement}>
+                    + Add Element(s)
+                </Button>
+                <Button className="btn-danger function-box-button" onClick={deleteSelectedElements}>
+                    - Delete Selected Element(s)
+                </Button>
+                <Button className="btn-danger function-box-button" onClick={clearCanvas}>
+                    Clear All
+                </Button>
+                <Button className="btn-info function-box-button" onClick={toggleRecord}>
+                    {displayRecord ? "Hide Record" : "Show Record"}
+                </Button>
                 {currentStructure === "graph" ? 
-                <Button className="btn-warning function-box-button" onClick={addGraphEdge}>Create Graph Edge</Button>  
+                    <Button className="btn-warning function-box-button" onClick={addGraphEdge}>
+                        <div className="function-box-button-header">Create Graph Edge</div>
+                        <div className="function-box-button-subtitle">(Between 2 selected nodes)</div>
+                    </Button>  
                 : null}                
                 <h3>Algorithms</h3>
                 {currentStructure ==="array" ?
-                <Button className="btn-success function-box-button" 
-                    onClick={() => {
-                        resetAlgoState()
-                        quickSort(0, elements.length - 1)
-                        if (!displayRecord) {toggleRecord()}
-                        setCurrentAlgorithm('quicksort')}}>
-                    Apply Quick Sort
-                </Button>
+                    <Button className="btn-success function-box-button" 
+                        onClick={() => {
+                            resetAlgoState()
+                            quickSort(0, elements.length - 1)
+                            if (!displayRecord) {toggleRecord()}
+                            setCurrentAlgorithm('quicksort')}}>
+                        Apply Quick Sort
+                    </Button>
                 : null}
                 {currentStructure ==="graph" ?
-                <Button className="btn-success function-box-button"
-                    onClick={() => {
-                        resetAlgoState()
-                        depthFirstSearch()
-                        if (!displayRecord) {toggleRecord()}
-                        setCurrentAlgorithm('DFS')
+                    <Button className="btn-success function-box-button"
+                        onClick={() => {
+                            resetAlgoState()
+                            depthFirstSearch()
+                            if (!displayRecord) {toggleRecord()}
+                            setCurrentAlgorithm('DFS')
 
-                    }}>
-                    Depth First Search
-                </Button>
+                        }}>
+                        <div className="function-box-button-header">Depth First Search</div>
+                        <div className="function-box-button-subtitle">(Selected node is starting node)</div>
+                    </Button>
                 : null}
-                <Button className="btn-warning function-box-button" onClick={() => showStep(true)}> Next Step</Button>
-                <Button className="btn-warning function-box-button" onClick={() => showStep(false)}> Previous Step</Button>
+                <Button className="btn-warning function-box-button" onClick={() => showStep(true)}>
+                    Next Step &rarr;
+                </Button>
+                <Button className="btn-warning function-box-button" onClick={() => showStep(false)}> 
+                    &larr; Previous Step
+                </Button>
             </div>
         </Draggable>
     )
